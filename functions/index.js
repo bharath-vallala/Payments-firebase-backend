733ab706ae05f02cefb5239855a12c288265ca80
Bharath <bharathvallala4@gmail.com>
2020-06-25 12:41:56 +0530

    first commit

--- functions/index.js
+++ functions/index.js
@@ -13,24 +13,24 @@ const stripe=new stripeloader(functions.config().s.key)
 
 // // Create and Deploy Your First Cloud Functions
  //https://firebase.google.com/docs/functions/write-firebase-functions
-const charge=(token,amount)=>{
-    return stripe.paymentIntents.create({
-        amount:parseInt(amount)*100,
-        currency: 'inr',
-        source:token,
-        metadata: {integration_check: 'accept_a_payment'},
-        description :" some des"
+ const chargew=(id,amt)=>{
+    stripe.charges.create({
+      amount:parseInt(amt)*100,
+      currency: "inr",
+      source:id,
+      description:'some des'
     })
-}
+  }
 
- app.post("/pay",(req,res)=>{
+ app.post("/pay",async(req,res)=>{
     
-      stripe.charges.charge(req.body.id,req.body.amount).then((data)=>{
-       return res.send(data);
-      }).catch((e)=>{
-
-            res.send(e);
-      })
+    try{
+        const result=await chargew(req.body.id,req.body.amount);
+        console.log(result);
+        res.send(result);
+      }catch (e) {
+        res.send(e)
+      }
        
         
     
